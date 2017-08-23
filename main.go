48a6fe3af7c883875fa8615aba3f37caefe8cefd
juraj <jurajkulich@gmail.com>
2017-08-23 09:43:24 +0200

    basic auth

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"github.com/labstack/echo/middleware"
 )
 
 type ToDoServer struct {
@@ -53,6 +54,10 @@ func (Todoserver *ToDoServer) deleteHandler (c echo.Context) error {
 	return c.JSON(http.StatusOK, dat)
 }
 
+func test(c echo.Context) error {
+	return c.String(http.StatusOK, "admin page")
+}
+
 func main() {
 	database, err := gorm.Open("sqlite3", "/tmp/gorm.db")
 	if err != nil {
@@ -69,8 +74,19 @@ func main() {
 	// todoserver.db.DropTable(ToDoItem{})
 
 	e := echo.New()
-	e.GET("/", todoserver.handler)
-	e.DELETE("/:id", todoserver.deleteHandler)
-	e.POST("/", todoserver.postHandler)
+	admin := e.Group("/admin")
+
+	admin.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "admin" && password == "admin" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	admin.GET("/test", test)
+	admin.GET("/", todoserver.handler)
+	admin.DELETE("/:id", todoserver.deleteHandler)
+	admin.POST("/", todoserver.postHandler)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
