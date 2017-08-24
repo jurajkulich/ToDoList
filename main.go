1dd0d0dd3ad28190db8564f7bcb04077a3f3e4b8
juraj <jurajkulich@gmail.com>
2017-08-24 13:40:32 +0200

    HasMany handlers

--- main.go
+++ main.go
@@ -11,12 +11,13 @@ import (
 	"errors"
 	"golang.org/x/crypto/scrypt"
 	"bytes"
+	"time"
+
 )
 
 const (
-	JSWKey = "muchWoW"
+	JWTKey = "muchWoW"
 	PSWDKey = "suchAmaze"
-
 )
 
 type ToDoServer struct {
@@ -28,14 +29,17 @@ func NewToDoServer(db *gorm.DB) *ToDoServer {
 }
 
 type ToDoItem struct {
-	ID 			uint `json:"id",gorm:"auto_increment"`
+	gorm.Model
 	IsDone      bool `json:"done"`
 	Description string `json:"description"`
+	UserID uint
 }
 
 type User struct {
+	gorm.Model
 	Username string `json:"username"`
 	Password []byte `json:"password"`
+	Todoitem []ToDoItem
 }
 
 type NameToken struct {
@@ -49,27 +53,45 @@ func (Todoserver *ToDoServer) GetDatabase() *gorm.DB {
 }
 
 func (Todoserver *ToDoServer) handler (c echo.Context) error {
-	var dat []ToDoItem
-	Todoserver.db.Find(&dat)
-	return c.JSON(http.StatusOK, dat)
+	user := &User{}
+	userName := c.Get("context")
+	err := Todoserver.db.First(user, "username = ?", userName).Error
+	if err != nil {
+		return c.JSON(http.StatusNoContent, err)
+	}
+	Todoserver.db.Model(&user).Related(&user.Todoitem)
+	return c.JSON(http.StatusOK, user)
 }
 
 func (Todoserver *ToDoServer) postHandler (c echo.Context) error {
 	todo := ToDoItem{}
 	c.Bind(&todo)
+	user := &User{}
+	userName := c.Get("context")
+	err := Todoserver.db.First(user, "username = ?", userName).Error
+	if err != nil {
+		return c.JSON(http.StatusNoContent, err)
+	}
+	todo.UserID = user.ID
 	Todoserver.db.Create(&todo)
-	var dat []ToDoItem
-	Todoserver.db.Find(&dat)
-	return c.JSON(http.StatusOK, dat)
+	return c.JSON(http.StatusOK, todo)
 }
 
 func (Todoserver *ToDoServer) deleteHandler (c echo.Context) error {
+	user := &User{}
+	userName := c.Get("context")
 	id := c.Param("id")
 	key, _ := strconv.Atoi(id)
-	Todoserver.db.Delete(ToDoItem{}, "id = ?", key)
-	var dat []ToDoItem
-	Todoserver.db.Find(&dat)
-	return c.JSON(http.StatusOK, dat)
+
+	todo := ToDoItem{}
+	err := Todoserver.db.First(user, "username = ?", userName).Error
+	if err != nil {
+		return c.JSON(http.StatusNoContent, err)
+	}
+	todo.UserID = user.ID
+
+	Todoserver.db.Delete(&todo, "id = ?", key)
+	return c.JSON(http.StatusOK, "Deleted")
 }
 
 func (Todoserver *ToDoServer) registerHandler (c echo.Context) error {
@@ -80,7 +102,7 @@ func (Todoserver *ToDoServer) registerHandler (c echo.Context) error {
 
 	if Todoserver.db.Where("username = ?", user.Username).First(&User{}).RecordNotFound() {
 		key, _ := scrypt.Key([]byte(user.Password), []byte(PSWDKey), 16384, 8, 1, 32)
-		dbUser := User {user.Username, key}
+		dbUser := User {Username: user.Username, Password:key}
 		Todoserver.db.Create(&dbUser)
 		return c.JSON(http.StatusOK, dbUser)
 	}
@@ -88,33 +110,58 @@ func (Todoserver *ToDoServer) registerHandler (c echo.Context) error {
 }
 
 func (Todoserver ToDoServer) loginHandler(c echo.Context) error {
+
+	// get user from POST
 	user := User{}
 	c.Bind(&user)
+	// crypt user password
 	key, _ := scrypt.Key(user.Password, []byte(PSWDKey), 16384, 8, 1, 32)
+
 	var dbUser User
 	if Todoserver.db.Where("username = ? ", user.Username).First(&dbUser).RecordNotFound() {
 		return c.JSON(http.StatusBadRequest, errors.New("Bad username or password!").Error())
 	}
 	if bytes.Compare(key, dbUser.Password) == 0 {
 		t := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"exp": time.Now().Add(time.Minute * 20).Unix(),
 			"username": dbUser.Username,
-			"password": dbUser.Password,
 		})
 
-		tokenString, _ := t.SignedString([]byte(JSWKey))
-		Todoserver.db.Create(&NameToken{dbUser.Username, tokenString})
-		fmt.Println(tokenString)
+		tokenString, _ := t.SignedString([]byte(JWTKey))
+		nameToken := &NameToken{dbUser.Username, tokenString}
+		Todoserver.db.Create(nameToken)
 
-		return c.JSON(http.StatusOK, dbUser)
+		return c.JSON(http.StatusOK, nameToken)
 	} else {
 		return c.JSON(http.StatusBadRequest, errors.New("Bad password!").Error())
 	}
 }
 
-func TokenToStruct(next echo.HandlerFunc) echo.HandlerFunc {
+func (Todoserver *ToDoServer) TokenToStruct(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		tokenString  := c.Request().Header.Get("authorization")
 
-		return nil
+		if Todoserver.db.Where("token = ? ", tokenString).First(&NameToken{}).RecordNotFound() {
+			return c.NoContent(http.StatusUnauthorized)
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(JWTKey), nil
+		})
+
+		claims,ok := token.Claims.(jwt.MapClaims)
+
+		if err == nil && token.Valid && ok {
+			username := claims["username"]
+			c.Set("context",  username)
+
+			fmt.Println("Token is valid")
+			return next(c)
+		} else {
+			fmt.Printf("Bad token")
+			return c.NoContent(http.StatusUnauthorized)
+		}
+
 	}
 
 }
@@ -124,18 +171,21 @@ func main() {
 	if err != nil {
 		panic("Can't connect to database")
 	}
+
+	// todoserver.db.DropTable(ToDoItem{})
+	// todoserver.db.DropTable(User{})
+	// todoserver.db.DropTable(NameToken{})
+
 	database.AutoMigrate(&ToDoItem{})
 	database.AutoMigrate(&User{})
 	database.AutoMigrate(&NameToken{})
 	defer database.Close()
 
 	todoserver := NewToDoServer(database)
-	// todoserver.db.DropTable(ToDoItem{})
-	// todoserver.db.DropTable(User{})
-	// todoserver.db.DropTable(NameToken{})
+
 	e := echo.New()
 	admin := e.Group("/admin")
-	admin.Use()
+	admin.Use(todoserver.TokenToStruct)
 
 	e.POST("/register", todoserver.registerHandler)
 	e.POST("/login", todoserver.loginHandler)
@@ -147,48 +197,3 @@ func main() {
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
-
-/*
-		tokenString := c.Response().Header().Get("Authorization")
-
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-			}
-			return []byte(SigningKey), nil
-		})
-		var user User
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			user = User { claims["username"].(string), claims["password"].(string)}
-		} else {
-			fmt.Println(err)
-		}
-
-		if err := Todoserver.db.Where("username = ? AND password = ?", user.Username, user.Password).First(User{}).Error; err != nil {
-			return c.NoContent(http.StatusUnauthorized)
-		}
-
-return next(c)
-
-*/
-
-/*
-
-
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(SigningKey), nil
-	})
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		fmt.Println(claims["username"], claims["password"])
-	} else {
-		fmt.Println(err)
-	}
-
-
-	return c.String(http.StatusOK, tokenString)
-
-	*/
